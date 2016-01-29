279729abd8b7ef083ee8edfb41a1144dd84ab691
LaPetiteSouris <sontunghoang@gmail.com>
2016-01-29 22:26:16 +0100

    using feature of previous day to predict next day result

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -4,9 +4,10 @@ import pandas.io.data as web
 
 def load_yahoo_finance_data():
     feature = []
+    cls_point_arr = []
     #current_date = date.datetime.now().date()
     start = date.datetime(2000, 1, 1)
-    current_date = date.datetime(2016, 1, 25)
+    current_date = date.datetime(2016, 1, 24)
     result = web.DataReader('ABBN.VX', 'yahoo', start, current_date)
     # Adj closing point of the index
     cls_point = result.loc[:, ['Close']]
@@ -14,20 +15,24 @@ def load_yahoo_finance_data():
     high_point = result.loc[:, ['High']]
     low_point = result.loc[:, ['Low']]
     vol = result.loc[:, ['Volume']]
-    for i in range(len(cls_point.index)):
+    for i in range((len(cls_point.index) - 1)):
         feature.append([open_point.iloc[i].values[0],
                         high_point.iloc[i].values[0],
                         low_point.iloc[i].values[0],
                         vol.iloc[i].values[0]]
                        )
-    return feature, cls_point
+    for i in range(len(cls_point.index)):
+        cls_point_arr.append(cls_point.iloc[i].values[0])
+    close_point_shifted_right = [cls_point_arr[i]
+                                 for i in range(1, len(cls_point_arr))]
+    return feature, close_point_shifted_right
 
 
 def load_raw_yahoo_finance_data():
     feature = []
     #current_date = date.datetime.now().date()
-    start = date.datetime(2016, 1, 26)
-    current_date = date.datetime(2016, 1, 27)
+    start = date.datetime(2016, 1, 25)
+    current_date = date.datetime(2016, 1, 28)
     result = web.DataReader('ABBN.VX', 'yahoo', start, current_date)
     # Adj closing point of the index
     cls_point = result.loc[:, ['Close']]
--- src/knn.py
+++ src/knn.py
@@ -7,17 +7,27 @@ class predictor:
         self.feature = [[]]
         self.result = []
 
-    def predict(self):
+    def predict(self, target):
         feature, result = fetcher.load_yahoo_finance_data()
         self.feature = feature
         self.result = result
-        knnregressor = KNeighborsRegressor(n_neighbors=5, weights='distance',
-                                           algorithm='kd_tree')
+        knnregressor = KNeighborsRegressor(n_neighbors=5, weights='uniform',
+                                           algorithm='brute')
         val = 0
         if feature and result is not None:
 
             knnregressor.fit(self.feature, self.result)
-            data = [16.95, 17.36, 16.84, 2573400]
-            data2 = [16.38, 16.99, 16.84, 2242100]
-            val = knnregressor.predict(data)
+            val = knnregressor.predict(target)
         return val
+
+p = predictor()
+data0 = [16.830, 16.83, 16.56, 1685700]
+data1 = [16.38, 16.99, 16.84, 2242100]
+data2 = [16.95, 17.36, 16.84, 2573400]
+data3 = [17.315, 17.51, 17.09, 2574800]
+p_26 = p.predict(data0)
+p_27 = p.predict(data1)
+p_28 = p.predict(data2)
+print 'predicted value 26/1 is %s' % p_26[0]
+print 'predicted value 27/1 is %s' % p_27[0]
+print 'predicted value 28/1 is %s' % p_28[0]
